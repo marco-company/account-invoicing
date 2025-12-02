253ed908294b882ac97a71be7a74b6c907486e03
Pablo Castelo <pablocv@gmx.es>
2025-12-02 14:16:53 +0100

    [MIG] account_invoice_analytic_search: Migration to 16.0

--- account_invoice_analytic_search/__manifest__.py
+++ account_invoice_analytic_search/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Invoice Analytic Search",
     "summary": "Search invoices by analytic account or by project manager",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Project Expert Team, ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-invoicing",
     "license": "AGPL-3",
